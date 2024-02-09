01dbbf44967f841e101eb1360ce4cf3700245ec7
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2024-02-09 02:29:07 +0000

    functions

---
+++ 08_functions.py
@@ -0,0 +1,17 @@
+# string functions
+str1="his is a string"
+str2=' This is also a string'
+print(str1.endswith("ing"))
+print(str1.startswith("This"))
+print(str1.capitalize())# His is a string
+print(str1.replace("a","pay")) #his is pay string
+print(str1.find("is")) #2
+print(str2.count("s"))
+
+
+#Q) WAP to input user name and print its length
+name=input("Enter your name: ");
+print("Length od name is =", len(name))
+
+
+

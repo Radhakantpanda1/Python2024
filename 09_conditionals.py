7bd3a561b5d54c7235a10d6ab688b16264e104ca
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2024-02-09 02:29:22 +0000

    conditional

---
+++ 09_conditionals.py
@@ -0,0 +1,22 @@
+#Conditional Statements
+# if else
+"""
+if(condition):
+    statement;
+else:
+  statement2:
+  """
+
+age=25;
+if(age>=18):
+  print("You can drive");
+else:
+  print("You can't drive");
+
+# elif
+
+# indentation- proper spacing
+
+# wap for grading system of students
+
+# nesting 
